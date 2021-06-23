c0e15405e51a1512953adf74383d4a97051cce7c
NastasiaLarina <stucha84@mail.ru>
2021-06-23 19:15:03 +0500

    new feature 2
    4. Add a second settings screen.

---
+++ TaskOne/Controller/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  TaskOne
+//
+//  Created by Анастасия Ларина on 23.06.2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
